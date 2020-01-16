f1ebb1d38742c5c6cd4179395f312be450ecc4d3
narumincho <narumincho.starfy@gmail.com>
2020-01-16 23:09:32 +0900

    read mic

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,48 @@
 #include <M5Stack.h>
-#include <driver/dac.h> //Arduino-ESP32 driver
 
-uint32_t beep_last_time = 0;
-uint8_t beep_volume = 20; //min 1, max 255
-uint32_t beep_total_time = 0;
+const int32_t micPin = 36;
+
+const int32_t micNumberOfSample = 500;
+const int32_t micSamplingFrequency = 40000;
+const uint32_t displayWidth = 320;
+const uint32_t displayHeight = 240;
+const uint8_t fontNumber = 2;
+const int16_t fontHeight = M5.Lcd.fontHeight(fontNumber);
 
 void setup()
 {
-    delay(2000);
-    beep_total_time = millis();
-
-    //これを実行すると、M5Stackスピーカーから破裂音が出る
-    dac_output_enable(DAC_CHANNEL_1); //DAC channel 1 is GPIO #25
+    M5.begin();
+    M5.Lcd.setBrightness(20);
+    M5.Lcd.setTextColor(TFT_ORANGE);
+    M5.Lcd.println("Read mic acc data and Send data on http. by narumincho");
+    pinMode(micPin, INPUT);
 }
 
 void loop()
 {
-    if (millis() - beep_total_time < 10000)
-    { //10秒後にbeep音停止
-        uint32_t b_period = millis() - beep_last_time;
-        if (b_period < 500)
-        {
-            dac_output_voltage(DAC_CHANNEL_1, 0);
-            delay(1); //約500Hz
-            //delayMicroseconds(500); //約1kHz
-            dac_output_voltage(DAC_CHANNEL_1, beep_volume);
-            delay(1); //約500Hz
-                      //delayMicroseconds(500); //約1kHz
-        }
-        else if (b_period >= 500 && b_period < 1000)
-        {
-            dac_output_voltage(DAC_CHANNEL_1, 0);
-        }
-        else
-        {
-            beep_last_time = millis();
-        }
-    }
-    else
-    {
-        //これを実行するとGPIO #25のDAC outputが無効になり、スピーカーからノイズが出なくなる。
-        //ただし、次にdac_output_enableを実行する時に破裂音が出る。
-        dac_output_disable(DAC_CHANNEL_1);
-        delay(100); //10秒無音状態
-        beep_total_time = millis();
+    uint16_t micBuffer[micNumberOfSample];
+    uint32_t sampling_period_us = round(1000000 * (1.0 / micSamplingFrequency));
 
-        //beep音を鳴らす為にはこれが必要。ただし破裂音が出る。
-        dac_output_enable(DAC_CHANNEL_1);
+    for (int i = 0; i < micNumberOfSample; i++)
+    {
+        uint64_t start = micros();
+        micBuffer[i] = analogRead(micPin);
+        while ((micros() - start) < sampling_period_us)
+        {
+        };
     }
+
+    int32_t sum = 0;
+    for (int i = 0; i < displayWidth; i++)
+    {
+        uint16_t data = micBuffer[i * micNumberOfSample / displayWidth];
+        M5.Lcd.drawLine(i, 8, i, 8 + 128, BLACK);
+        M5.Lcd.drawLine(i, 8 + 128 / 2, i, 8 + data / 2, WHITE);
+        sum += abs(data - 128);
+    }
+    M5.Lcd.setCursor(0, 220);
+    M5.Lcd.fillRect(0, 220, 7 * 6, fontHeight, BLACK);
+    M5.Lcd.setTextColor(WHITE);
+    M5.Lcd.printf("% 7d", sum);
+    delay(16);
 }
